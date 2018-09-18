ba0d61ab6bcbfdea501a18180832302b06bd527c
Richard Sanchez <richard.charles.s@gmail.com>
2018-09-18 16:03:15 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,6 +39,11 @@ class Student
       WHERE grade < 12
     SQL
 
+    DB[:conn].execute(sql).map do |row|
+     self.new_from_db(row)
+   end
+ end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
